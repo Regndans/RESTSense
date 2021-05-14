2ce2517210aabb3d251986c5833c82cebef28dcd
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2021-05-14 11:01:28 +0200

    Timer Lavet
    Indsat controller manager og model i koden, samt lavet test til timer database tabel

---
+++ RESTSense/Managers/TimerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RESTSense.Model;
+
+namespace RESTSense.Managers
+{
+    public class TimerManager
+    {
+        private readonly PirContext _context;
+
+        public TimerManager(PirContext context)
+        {
+            _context = context;
+        }
+
+        public List<TimerModel> GetAll()
+        {
+            return _context.ActiveTime.ToList();
+        }
+
+        public TimerModel UpdateTimer(int v, TimerModel newTime)
+        {
+            TimerModel toChange = _context.ActiveTime.Find(v);
+            if (toChange == null) return null;
+            toChange.ActiveEnd = newTime.ActiveEnd;
+            toChange.ActiveStart = newTime.ActiveStart;
+            toChange.TimerActive = newTime.TimerActive;
+            _context.SaveChanges();
+            return toChange;
+        }
+    }
+}
---
+++ RESTSenseTests/Managers/TimerManagerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RESTSense;
+using RESTSense.Managers;
+using RESTSense.Model;
+
+namespace RESTSenseTests.Managers
+{
+    [TestClass]
+    public class TimerManagerTest
+    {
+        private TimerManager _manager;
+        private readonly PirContext _context;
+        public TimerManagerTest()
+        {
+            DbContextOptionsBuilder<PirContext> options = new DbContextOptionsBuilder<PirContext>();
+            options.UseSqlServer(Secrets.ConnectionString);
+            _context = new PirContext(options.Options);
+        }
+
+        [TestInitialize]
+        public void Init()
+        {
+            _manager = new TimerManager(_context);
+        }
+        [TestMethod]
+        public void ManagerTimerGetAllTest()
+        {
+            List<TimerModel> TimerList = _manager.GetAll();
+            int sizeOfList = TimerList.Count();
+            Console.WriteLine(sizeOfList);
+            Assert.AreEqual(sizeOfList, TimerList.Count);
+
+        }
+
+        [TestMethod]
+        public void ManagerTimerPutTest()
+        {
+            List<TimerModel> TimerList = _manager.GetAll();
+            TimerModel newTime = new TimerModel();
+            newTime = TimerList[0];
+            newTime.ActiveEnd = DateTime.Now;
+
+            _manager.UpdateTimer(1, newTime);
+            TimerList = _manager.GetAll();
+            Assert.AreEqual(newTime.ActiveEnd,TimerList[0].ActiveEnd);
+
+        }
+    }
+}
